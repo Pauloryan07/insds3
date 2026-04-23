eb28e0ec8366d38d419ca5425ec8ad88bcd98246
pauloryan07 <pauloryan418@gmail.com>
2026-04-23 08:13:13 -0300

    Correeção das tabela do bd

--- database/migrations/2026_04_20_165712_create_freefires_table.php
+++ database/migrations/2026_04_20_165712_create_freefires_table.php
@@ -4,19 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
 
-   public function up(): void
+    public function up(): void
     {
-     
+
         Schema::create('freefire', function (Blueprint $table) {
             $table->id();
             $table->string('nick');
-            $table->string('player_id'); 
+            $table->string('name_team')->default('sem_time');
+            $table->string('name_members')->default('sem_nome');
+            $table->string('player_id');
+            $table->string('members_id')->default('0');
             $table->timestamps();
         });
     }
